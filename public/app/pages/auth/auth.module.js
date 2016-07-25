2e57f774d12833cfcc3724a8d933290863b4201a
vain <sticver@gmail.com>
2016-07-25 14:37:13 +0800

    add login and reg model

---
+++ public/app/pages/auth/auth.module.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chaclus on 16/7/25.
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('Hunter.pages.auth', [
+        'Hunter.pages.auth.login',
+        'Hunter.pages.auth.reg'
+    ])
+})();
---
+++ public/app/pages/auth/login/LoginPageCtrl.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chaclus on 16/7/25.
+ */
+
+
+(function () {
+    'use strict';
+
+    angular.module('Hunter.pages.auth.login')
+        .controller('LoginPageCtrl', LoginPageCtrl);
+
+    function LoginPageCtrl($scope, $http, toastr, fileReader, $filter, $uibModal) {
+        
+    }
+})();
